32d66f8a10f54d7f4693fa78bd8c5377729781f2
unknown <tohjbin2@gmail.com>
2022-02-15 14:22:18 +0900

    WIP

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 function index() {
+  export async function getStaticProps() {
+    const API = '';
+    const res = await Axios.get();
+    const data = res.data;
+
+    return {
+      props: {},
+    };
+  }
   return <div>index</div>;
 }
 
---
+++ pages/items/index.style.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ItemsSection = styled.div``;
+
+export const BarSection = styled.div``;
+
+export const ContentsSection = styled.div``;
